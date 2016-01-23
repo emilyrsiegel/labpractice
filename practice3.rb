aaa835720b0f43750fddffe1c00d02d6b826b922
emilyrsiegel <emilysiegel97@gmail.com>
2016-01-23 13:50:22 -0600

    Add an App Idea

--- practice3.rb
+++ practice3.rb
@@ -2,3 +2,4 @@ Name: Emily
 Expectation: I want to learn how to code!
 Occupation: Project Manager
 Interesting Fact: I am from Connecticut
+App Idea: An app that gives me real time subway information!
